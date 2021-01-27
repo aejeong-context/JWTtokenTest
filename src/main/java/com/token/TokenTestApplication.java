4595dff87248c85553a9e45b283f2afd98e5a551
aejeong <aejeong-context@kakao.com>
2021-01-27 17:10:10 +0900

    [init] token 구현을 위한 프로젝트 생성
    SpringBoot, Spring Security, JWT 의존성 추가 등 프로젝트 구조 생성

---
+++ src/main/java/com/token/TokenTestApplication.java
@@ -0,0 +1,13 @@
+package com.token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TokenTestApplication.class, args);
+    }
+
+}
+
