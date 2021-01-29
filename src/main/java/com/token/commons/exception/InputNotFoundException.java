3e837a5561a73c50f0b76388f5b4e4b0aa088ea6
aejeong <aejeong-context@kakao.com>
2021-01-29 19:55:17 +0900

    [rmv] Security 삭제, 불필요 class 삭제

--- src/main/java/com/token/commons/exception/InputNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.token.commons.exception;
-
-// 아이디 비밀번호 전달 오류시 예외처리 클래스
-public class InputNotFoundException extends RuntimeException{
-    public InputNotFoundException(){
-        super();
-    }
-}
